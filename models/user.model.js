62472a4c5f31a79ef63ea06b2767df61946db439
kvoncode <kvoncode@gmail.com>
2020-05-11 20:06:37 +0900

    add: Joi schema and validation, jwt generation

--- models/user.model.js
+++ models/user.model.js
@@ -24,5 +24,29 @@ const UserSchema = new mongoose.Schema({
     maxlength: 255,
   },
 
-  idAdmin: Boolean
+  isAdmin: Boolean,
 });
+
+UserSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("myprivatekey")
+  );
+
+  return token;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(3).max(255).required,
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
