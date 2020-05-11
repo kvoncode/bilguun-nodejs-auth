b58df4a52190d4d1d087495ce93c011eea9de208
kvoncode <kvoncode@gmail.com>
2020-05-11 19:58:40 +0900

    add: user mongoose schema

---
+++ models/user.model.js
@@ -0,0 +1,28 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255,
+  },
+
+  idAdmin: Boolean
+});
